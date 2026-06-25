634d8572e433bad54e5b671191ee3de89ff83dc5
Silver7-7 <bry13espinoza@gmail.com>
2026-06-25 04:07:18 -0500

    modified

--- SmartSACH-FDT/Frontend/src/main.jsx
+++ SmartSACH-FDT/Frontend/src/main.jsx
@@ -1,11 +1,13 @@
+// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom'; // 1. Importamos el BrowserRouter
 import App from './App.jsx';
 import './assets/css/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* 2. Envolvemos toda la aplicación aquí */}
     <BrowserRouter>
       <App />
     </BrowserRouter>
