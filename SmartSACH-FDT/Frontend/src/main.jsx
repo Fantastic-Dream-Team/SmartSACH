2d0522f4935b1593311e1a95f696a588b8065289
Silver7-7 <bry13espinoza@gmail.com>
2026-06-25 03:27:00 -0500

    fixes dashboard

--- SmartSACH-FDT/Frontend/src/main.jsx
+++ SmartSACH-FDT/Frontend/src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
-// Si tienes estilos CSS globales o personalizados de tu rama, los importas aquí:
 import './assets/css/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
